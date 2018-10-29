69d6ea8c67d9dfc97257eb526b304d8428a591a5
Ioannis Papatsoris <giannhs95@yahoo.gr>
2018-10-29 05:01:26 +0200

    Updates

---
+++ precache-manifest.92a644de29cb0c42961db1a2838c2777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44eef3209494367c536f",
+    "url": "/Kill-Death-Ratio/static/js/runtime~main.44eef320.js"
+  },
+  {
+    "revision": "b19319fb7475fa1fc813",
+    "url": "/Kill-Death-Ratio/static/js/main.b19319fb.chunk.js"
+  },
+  {
+    "revision": "1e91c213d90f65c6e9b9",
+    "url": "/Kill-Death-Ratio/static/js/1.1e91c213.chunk.js"
+  },
+  {
+    "revision": "b19319fb7475fa1fc813",
+    "url": "/Kill-Death-Ratio/static/css/main.c2ed4dc7.chunk.css"
+  },
+  {
+    "revision": "245bf78dbaf834948ba8685d6e55f175",
+    "url": "/Kill-Death-Ratio/index.html"
+  }
+];
